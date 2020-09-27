0ae93ddc85e3a7048de1015c01ba07f686e74ad7
dgrajewski <dawids115@gmail.com>
2020-09-27 22:09:38 +0200

    Added tests for nested registrations;

---
+++ Unit-Tests/ContainerGetNested.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unit_Tests.Models;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class ContainerGetNested : ContainerBaseTest
+    {
+        [TestMethod]
+        public void HandlesNestedDependenciesRegisteredInLogicalOrder()
+        {
+            Container.Single<IMockDependency>(() => new MockDepenedency());
+            Container.Factory<IMockDependency>("", () => new MockDepenedency(Container.Get<IMockDependency>()));
+
+            var dependency = Container.Get<IMockDependency>("");
+
+            Assert.AreNotEqual(null, dependency.Inner);
+            Assert.AreEqual(null, dependency.Inner.Inner);
+        }
+
+        [TestMethod]
+        public void HandlesNestedDependenciesRegisteredInNotLogicalOrder()
+        {
+            Container.Single<IMockDependency>("", () => new MockDepenedency(Container.Get<IMockDependency>()));
+            Container.Factory<IMockDependency>(() => new MockDepenedency());
+
+            var dependency = Container.Get<IMockDependency>("");
+
+            Assert.AreNotEqual(null, dependency.Inner);
+            Assert.AreEqual(null, dependency.Inner.Inner);
+        }
+    }
+}
--- Unit-Tests/Models/MockDependency.cs
+++ Unit-Tests/Models/MockDependency.cs
@@ -2,11 +2,19 @@
 {
     internal interface IMockDependency
     {
+        IMockDependency Inner { get; }
         void DoSomething();
     }
 
     internal class MockDepenedency : IMockDependency
     {
+        public IMockDependency Inner { get; }
+
+        public MockDepenedency(IMockDependency inner = null)
+        {
+            Inner = inner;
+        }
+
         public void DoSomething()
         {
 
