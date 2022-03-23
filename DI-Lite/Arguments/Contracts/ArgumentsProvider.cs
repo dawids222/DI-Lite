43a2c7760270b5ae046e14223d2a93648efdea58
dgrajewski <dawids115@gmail.com>
2022-03-23 19:29:51 +0100

    Added ArgumentsProvider contracts

---
+++ DI-Lite/Arguments/Contracts/ArgumentsProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DI_Lite.Arguments.Contracts
+{
+    public abstract class ArgumentsProvider : IArgumentsProvider
+    {
+        public T Get<T>(string name) => (T)Get(typeof(T), name);
+        public abstract object Get(Type type, string name);
+
+        public bool Contains<T>(string name) => Contains(typeof(T), name);
+        public abstract bool Contains(Type type, string name);
+    }
+}
