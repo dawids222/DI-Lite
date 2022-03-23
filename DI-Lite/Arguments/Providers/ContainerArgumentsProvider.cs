7ae19c65ab330ca64f7c6ed9ad56203ad3e80519
dgrajewski <dawids115@gmail.com>
2022-03-23 19:30:05 +0100

    Added ContainerArgumentsProvider

---
+++ DI-Lite/Arguments/Providers/ContainerArgumentsProvider.cs
@@ -0,0 +1,19 @@
+﻿using DI_Lite.Arguments.Contracts;
+using System;
+
+namespace DI_Lite.Arguments.Providers
+{
+    public class ContainerArgumentsProvider : ArgumentsProvider
+    {
+        private readonly IDependencyProvider _container;
+
+        public ContainerArgumentsProvider(IDependencyProvider dependencyProvider)
+        {
+            _container = dependencyProvider;
+        }
+
+        public override object Get(Type type, string name) => _container.Get(type);
+
+        public override bool Contains(Type type, string name) => _container.Contains(type);
+    }
+}
