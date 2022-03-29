5cd98708e83d42d40954569ab276f79fef4c19b0
dgrajewski <dawids115@gmail.com>
2022-03-29 20:20:13 +0200

    Added DelegateInvoker

---
+++ DI-Lite/Arguments/DelegateInvoker.cs
@@ -0,0 +1,40 @@
+﻿using DI_Lite.Arguments.Contracts;
+using DI_Lite.Extensions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI_Lite.Arguments
+{
+    public class DelegateInvoker
+    {
+        private readonly Delegate _delegate;
+        private readonly IArgumentsProvider _provider;
+
+        public DelegateInvoker(Delegate del, IArgumentsProvider provider)
+        {
+            _delegate = del;
+            _provider = provider;
+        }
+
+        public async Task<object> InvokeAsync()
+        {
+            var args = GetArguments();
+            return await _delegate.DynamicInvokeAsync(args);
+        }
+
+        public object Invoke()
+        {
+            var args = GetArguments();
+            return _delegate.DynamicInvoke(args);
+        }
+
+        private object[] GetArguments()
+        {
+            return _delegate.Method
+                .GetParameters()
+                .Select(p => _provider.Get(p.ParameterType, p.Name))
+                .ToArray();
+        }
+    }
+}
